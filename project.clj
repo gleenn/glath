bed590ddfc4ec8857eb4f88ce4ee75ea2fcb56e4
Glenn Jahnke <gleenn@gmail.com>
2017-03-21 21:34:04 -0700

    upgrade to clojure; install test refresh

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject glath "0.1.0-SNAPSHOT"
   :description "Clojure library that implements some basic math functions"
   :url "https://github.com/gleenn/glath"
-  :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/test.check "0.9.0" :scope "test"]]
+
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]}})
