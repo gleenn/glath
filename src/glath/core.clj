05608718a3f1c4f0f946e887428fb612aef2f423
Glenn Jahnke <gleenn@gmail.com>
2017-10-09 16:10:53 -0700

    reclassify fn works with test

--- src/glath/core.clj
+++ src/glath/core.clj
@@ -27,7 +27,7 @@
   (->> points
        transpose
        (map (juxt #(apply min %) #(apply max %)))
-       (map (fn [[min max]] (+ min (/ (- max min) 2))))))
+       (map (fn [[min max]] (+ min (double (/ (- max min) 2)))))))
 
 (defn initial-classifications [points n-groups]
   (->> points
@@ -37,18 +37,35 @@
        (apply hash-map)))
 
 (defn reclassify [classification]
-  classification)
+  "Classification is a map where the key is the center (median-point) in N-dimensional space
+   and the values are the set of points that are in that center's group. This function re-arranges
+   points so they are in the group that has the closest median-point, then recalculates the
+   median-points for each group."
+  (let [median-points (keys classification)
+        all-points (apply concat (vals classification))
+        re-grouped-classification
+        (reduce (fn [new-classification point]
+                  (let [closest-median-point (apply min-key #(cartesian-distance point %) median-points)]
+                    (println "point" point
+                             "is closest to" closest-median-point
+                             "because its distance was" (cartesian-distance point closest-median-point))
+                    (update new-classification
+                            closest-median-point
+                            (fnil #(conj % point) (hash-set)))))
+                {} all-points)]
+    (reduce (fn [result [_ new-group]]
+              (assoc result (median-point new-group) new-group)) {}
+            re-grouped-classification)))
 
 #_(defn k-means [points n-groups]
-  ; take points and assign them to initial groups
-  ; [[0 0 1] [0 1 0] [1 0 0] [0 0 2] [0 2 0] [2 0 0]] =>
-  ; 1. map of group-index G_i => random non-overlapping set of points
-  ; 2. calculate median point for each group based on points contained in G_i for each i
-  ; 3. a) calculate distance from G_i to each point
-  ;    b) select G_i that has min distance as
-  ;    c) reassign every point to new group-index based on which G_i is closest
-  ; 4. goto step 2
-
+  "
+  - take points and assign them to initial groups randomly
+  - calculate median point for each group based on points contained in G_i for each i
+  - a) calculate distance from G_i to each point
+    b) select G_i that has min distance as
+    c) reassign every point to new group-index based on which G_i is closest
+  - goto step 2 if things have moved
+  "
   (let [groups-medians (group-medians)]
     (loop []))
   )
--- test/glath/core_test.clj
+++ test/glath/core_test.clj
@@ -66,5 +66,23 @@
   (is (= (Math/sqrt 14.0) (cartesian-distance [3.0 2.0 1.0] [0.0 0.0 0.0 0.0]))))
 
 (deftest reclassify-test
-  (is (= {} (reclassify {})))
-  (is (= {[1.0 0.0 0.0] [[1.0 0.0 0.0]]} (reclassify {[1.0 0.0 0.0] [[1.0 0.0 0.0]]}))))
+  (testing "super basic cases"
+    (is (= {} (reclassify {})))
+    (is (= {[1.0 0.0 0.0] #{[1.0 0.0 0.0]}}
+           (reclassify {[1.0 0.0 0.0] #{[1.0 0.0 0.0]}}))))
+
+  (testing "moves points to the median-point that is closest"
+    (is (=
+          {[2.5] #{[2] [3]}}
+
+          (reclassify {[10] #{[2] [3]}
+                       [1]  #{}})))
+
+    (is (= #_{[1.5 0.0] [[1.0 0.0]]
+              [0.0 3.0] [[0.0 1.0]]}
+
+          {[3.0 0.0] #{[1.0 0.0] [5.0 0.0]}
+           [0.0 1.5] #{[0.0 1.0] [0.0 2.0]}}
+
+          (reclassify {[0.9 0.0] #{[1.0 0.0] [0.0 2.0]}
+                       [0.0 0.9] #{[0.0 1.0] [5.0 0.0]}})))))
